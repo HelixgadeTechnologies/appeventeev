a4debad1b811024cbf381e66f38462181ff809d6
estherchinda <davidakachi9@gmail.com>
2025-04-09 16:51:44 +0100

    fixed error in loader

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -79,7 +79,7 @@ const SignIn = () => {
 
         navigate("/all-events");
       }
-      window.location.reload();
+      // window.location.reload();
     } catch (error) {
    //   toast.error(error.response?.data?.message || "Login failed. Please try again.");
       toast({
